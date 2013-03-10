88122bc4e07259621f71a2352960d6ed9d97b073
Martin Brochhaus <mbrochh@gmail.com>
2013-03-10 14:03:56 +0800

    Added templates to CMS_TEMPLATES setting
    This way we can re-use the templates and content we already created with
    the symposion cms.

--- pyconsg/pyconsg/settings/installed_apps/cms.py
+++ pyconsg/pyconsg/settings/installed_apps/cms.py
@@ -1,6 +1,10 @@
 """Settings for the ``django-cms`` app."""
 CMS_TEMPLATES = (
+    ('homepage.html', 'Home'),
     ('news.html', 'News'),
+    ('venue.html', 'Venue'),
+    ('proposals.html', 'Proposals'),
+    ('sponsorship.html', 'Sponsorship'),
 )
 
 JQUERY_JS = 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js'
