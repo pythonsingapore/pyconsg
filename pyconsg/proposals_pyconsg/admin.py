fe9f194ad78f1acb93ad09b87c75938df79dfb5a
Calvin Cheng <linchuan.cheng@gmail.com>
2013-04-30 10:10:05 +0800

    Update list_display to be admin-friendly

--- pyconsg/proposals_pyconsg/admin.py
+++ pyconsg/proposals_pyconsg/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 from proposals_pyconsg.models import (
     TalkProposal,
@@ -8,15 +10,33 @@ from proposals_pyconsg.models import (
 
 
 class TalkProposalAdmin(admin.ModelAdmin):
-    list_display = ['title', 'speaker', 'submitted']
+    list_display = ['title', 'speaker', 'speaker_email', 'submitted']
+
+    def speaker_email(self, obj):
+        u = User.objects.get(email=obj.speaker.email)
+        url = reverse("admin:%s_%s_change" % (u._meta.app_label, u._meta.module_name), args=(u.pk,))
+        return '<a href="%s">%s<a/>' % (url, obj.speaker.email)
+    speaker_email.allow_tags = True
 
 
 class TutorialProposalAdmin(admin.ModelAdmin):
-    list_display = ['title', 'speaker', 'submitted']
+    list_display = ['title', 'speaker', 'speaker_email', 'submitted']
+
+    def speaker_email(self, obj):
+        u = User.objects.get(email=obj.speaker.email)
+        url = reverse("admin:%s_%s_change" % (u._meta.app_label, u._meta.module_name), args=(u.pk,))
+        return '<a href="%s">%s<a/>' % (url, obj.speaker.email)
+    speaker_email.allow_tags = True
 
 
 class StarupBoothProposalAdmin(admin.ModelAdmin):
-    list_display = ['title', 'speaker', 'submitted']
+    list_display = ['title', 'speaker', 'speaker_email', 'submitted']
+
+    def speaker_email(self, obj):
+        u = User.objects.get(email=obj.speaker.email)
+        url = reverse("admin:%s_%s_change" % (u._meta.app_label, u._meta.module_name), args=(u.pk,))
+        return '<a href="%s">%s<a/>' % (url, obj.speaker.email)
+    speaker_email.allow_tags = True
 
 
 admin.site.register(TalkProposal, TalkProposalAdmin)
