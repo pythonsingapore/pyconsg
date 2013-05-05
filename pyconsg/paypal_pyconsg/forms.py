d9d766aac644767733edf2fd9e23965e36a5d350
Martin Brochhaus <mbrochh@gmail.com>
2013-05-05 23:13:48 +0800

    First step of enhancing checkout form
    Next I need to save the tshirt and tutorial selections and add JS magic
    to show student rates when checkbox is clicked.

--- pyconsg/paypal_pyconsg/forms.py
+++ pyconsg/paypal_pyconsg/forms.py
@@ -2,51 +2,81 @@
 Forms for the ``paypal_express_checkout`` app in the ``pyconsg`` context.
 
 """
+import datetime
+
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+
+from symposion.schedule.models import Presentation
 
 from paypal_express_checkout.models import Item
 from paypal_express_checkout.forms import SetExpressCheckoutFormMixin
 
 
+TSHIRT_CHOICES = (
+    ('XS', 'XS'),
+    ('S', 'S'),
+    ('L', 'L'),
+    ('XL', 'XL'),
+    ('XXL', 'XXL'),
+    ('XXXL', 'XXXL'),
+)
+
+
 class PyconsgSetExpressCheckoutForm(SetExpressCheckoutFormMixin):
-    conference_tickets = forms.IntegerField(
-        min_value=0,
-        label=_('Conference tickets'),
-        initial=1,
-        widget=forms.TextInput(
+    student_rate = forms.BooleanField(
+        label=_('I am a student'),
+        initial=False,
+        widget=forms.CheckboxInput(
             attrs={'style': 'width: 2em; float: left; margin-right: 1em;', }),
+        required=False,
     )
 
-    student_tickets = forms.IntegerField(
-        min_value=0,
-        label=_('Conference tickets (Student rate)'),
-        initial=0,
-        widget=forms.TextInput(
+    conference_ticket = forms.BooleanField(
+        label=_('Conference ticket'),
+        initial=True,
+        widget=forms.CheckboxInput(
             attrs={'style': 'width: 2em; float: left; margin-right: 1em;', }),
+        required=False,
     )
 
-    tutorial_tickets = forms.IntegerField(
-        min_value=0,
-        label=_('Tutorial tickets'),
-        initial=0,
-        widget=forms.TextInput(
-            attrs={'style': 'width: 2em; float: left; margin-right: 1em;', }),
+    tutorial_morning = forms.ModelChoiceField(
+        label=_('Tutorial (morning)'),
+        queryset=Presentation.objects.filter(
+            proposal_base__kind__slug='tutorial',
+            slot__start__lte=datetime.time(13, 0)),
+        required=False,
+    )
+
+    tutorial_afternoon = forms.ModelChoiceField(
+        label=_('Tutorial (afternoon)'),
+        queryset=Presentation.objects.filter(
+            proposal_base__kind__slug='tutorial',
+            slot__start__gt=datetime.time(13, 0)),
+        required=False,
+    )
+
+    tshirt_size = forms.ChoiceField(
+        label=_('T-Shirt size'),
+        choices=TSHIRT_CHOICES,
+        initial='L',
     )
 
     def __init__(self, *args, **kwargs):
         super(PyconsgSetExpressCheckoutForm, self).__init__(*args, **kwargs)
         self.conference_item = Item.objects.get(identifier='conference-early')
-        self.student_item = Item.objects.get(identifier='conference-student')
-        self.tutorial_item = Item.objects.get(identifier='tutorial')
+        self.student_item = Item.objects.get(
+            identifier='conference-student-early')
+        self.tutorial_item = Item.objects.get(identifier='tutorial-early')
 
     def clean(self):
         data = self.cleaned_data
-        if (data.get('conference_tickets', 0) == 0
-                and data.get('tutorial_tickets', 0) == 0
-                and data.get('student_tickets', 0) == 0):
+        if (not data.get('conference_ticket')
+                and not data.get('tutorial_morning')
+                and not data.get('tutorial_afternoon')):
             raise forms.ValidationError(
-                'You have to buy at least one ticket.')
+                'You have to select at least one conference ticket or one'
+                ' tutorial.')
         return data
 
     def get_items_and_quantities(self):
@@ -58,10 +88,15 @@ class PyconsgSetExpressCheckoutForm(SetExpressCheckoutFormMixin):
         """
         data = self.cleaned_data
         result = []
-        if data['conference_tickets'] > 0:
-            result.append((self.conference_item, data['conference_tickets']))
-        if data['student_tickets'] > 0:
-            result.append((self.student_item, data['student_tickets']))
-        if data['tutorial_tickets'] > 0:
-            result.append((self.tutorial_item, data['tutorial_tickets']))
+        if data['conference_ticket'] and not data['student_rate']:
+            result.append((self.conference_item, 1))
+        if data['conference_ticket'] and data['student_rate']:
+            result.append((self.student_item, 1))
+        tutorial_amount = 0
+        if data['tutorial_morning']:
+            tutorial_amount += 1
+        if data['tutorial_afternoon']:
+            tutorial_amount += 1
+        if tutorial_amount:
+            result.append((self.tutorial_item, tutorial_amount))
         return result
