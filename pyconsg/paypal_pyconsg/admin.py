4f33c3afd56819fa9ee998787fbfef6a057900e8
Martin Brochhaus <mbrochh@gmail.com>
2013-05-31 18:43:22 +0800

    Added transaction status and list filter to checkout choices admin

--- pyconsg/paypal_pyconsg/admin.py
+++ pyconsg/paypal_pyconsg/admin.py
@@ -7,9 +7,10 @@ from .models import CheckoutChoices
 class CheckoutChoicesAdmin(admin.ModelAdmin):
     model = CheckoutChoices
     list_display = [
-        'user', 'user__email', 'transaction', 'is_student',
-        'has_conference_ticket', 'tutorial_morning', 'tutorial_afternoon',
-        'tshirt_size', ]
+        'user', 'user__email', 'transaction', 'transaction__status',
+        'is_student', 'has_conference_ticket', 'tutorial_morning',
+        'tutorial_afternoon', 'tshirt_size', ]
+    list_filter = ['transaction__status', ]
     search_fields = [
         'user__email', 'user__first_name', 'user__last_name',
         'transaction__transaction_id', 'tutorial_morning__title',
@@ -18,6 +19,9 @@ class CheckoutChoicesAdmin(admin.ModelAdmin):
 
     def user__email(self, obj):
         return obj.user.email
+
+    def transaction__status(self, obj):
+        return obj.transaction.status
 
 
 admin.site.register(CheckoutChoices, CheckoutChoicesAdmin)
